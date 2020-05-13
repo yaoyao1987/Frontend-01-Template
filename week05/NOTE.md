ac6d2b79bb9572d50e02e4df5ace3672da98f7af
yaoyao1987 <yaolingyan1987@126.com>
2020-05-13 17:38:42 +0800

    feat: 提交week05作业
        feat(新特性),
        fix(修改问题),
        docs(文档修改),
        style(代码格式修改, 注意不是 css 修改),
        refactor(代码重构),
        test(测试),
        chore(其他修改, 比如构建流程, 依赖管理)
        影响的的范围
        提交描述

--- week05/NOTE.md
+++ week05/NOTE.md
@@ -1,1 +1,25 @@
-# 每周总结可以写在这里
+# 第五周
+
+## 本周作业
+
+1. 观看“浏览器工作原理 | HTTP 协议 + 语法与词法分析（二）”，根据老师课上讲解，完成课堂上的代码
+   [client.js](./toy-browser/client.js)
+   [server.js](./toy-browser/server.js)
+
+## 本周学习总结
+
+### 浏览器是如何工作的
+
+![浏览器工作过程](./images/url.png)
+
+### OSI 七层模型与 TCP/IP 四层模型对比
+
+![网络模型](./images/网络模型.png)
+
+### HTTP 协议的结构
+
+用 toy-browser 分析了 HTTP 协议的结构
+
+HTTP 协议格式：
+
+![HTTP格式](./images/HTTP格式.png)
---
+++ week05/Realm.js
@@ -0,0 +1,92 @@
+let set = new Set();
+const globalProperties = [
+  "eval",
+  "isFinite",
+  "isNaN",
+  "parseFloat",
+  "parseInt",
+  "decodeURI",
+  "decodeURIComponent",
+  "encodeURI",
+  "encodeURIComponent",
+  "Array",
+  "Date",
+  "RegExp",
+  "Promise",
+  "Proxy",
+  "Map",
+  "WeakMap",
+  "Set",
+  "WeakSet",
+  "Function",
+  "Boolean",
+  "String",
+  "Number",
+  "Symbol",
+  "Object",
+  "Error",
+  "EvalError",
+  "RangeError",
+  "ReferenceError",
+  "SyntaxError",
+  "TypeError",
+  "URIError",
+  "ArrayBuffer",
+  "SharedArrayBuffer",
+  "DataView",
+  "Float32Array",
+  "Float64Array",
+  "Int8Array",
+  "Int16Array",
+  "Int32Array",
+  "Uint8Array",
+  "Uint16Array",
+  "Uint32Array",
+  "Uint8ClampedArray",
+  "Atomics",
+  "JSON",
+  "Math",
+  "Reflect",
+];
+
+let queue = [];
+for (let q of globalProperties) {
+  queue.push({
+    path: [q],
+    object: this[q],
+  });
+}
+
+let current;
+while (queue.length) {
+  current = queue.shift();
+  if (set.has(current.object)) continue;
+  set.add(current.object);
+
+  for (let p of Object.getOwnPropertyNames(current.object)) {
+    var property = Object.getOwnPropertyDescriptor(current.object, p);
+    if (
+      property.hasOwnProperty("value") &&
+      ((property.value != null && typeof property.value == "object") ||
+        typeof property.value == "function") &&
+      property.value instanceof Object
+    ) {
+      queue.push({
+        path: current.path.concat([p]),
+        object: property.value,
+      });
+    }
+    if (property.hasOwnProperty("get") && typeof property.get == "function") {
+      queue.push({
+        path: current.path.concat([p]),
+        object: property.get,
+      });
+    }
+    if (property.hasOwnProperty("set") && typeof property.set == "function") {
+      queue.push({
+        path: current.path.concat([p]),
+        object: property.set,
+      });
+    }
+  }
+}
