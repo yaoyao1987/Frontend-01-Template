5f9bda4c8170e5599584da742042066153a62a70
yaoyao1987 <yaolingyan1987@126.com>
2020-06-25 13:27:06 +0800

    feat: 提交week11作业
    <body>
    
    <footer>
    
        feat(新特性),
        fix(修改问题),
        docs(文档修改),
        style(代码格式修改, 注意不是 css 修改),
        refactor(代码重构),
        test(测试),
        chore(其他修改, 比如构建流程, 依赖管理)
        影响的的范围
        提交描述

--- week10/1.js
+++ week10/1.js
@@ -1,165 +1,160 @@
-,"MediaCapabilities"
-,"InputDeviceInfo"
-,"DecompressionStream"
-,"CompressionStream"
-,"BlobEvent"
-,"BeforeInstallPromptEvent"
-,"BaseAudioContext"
-,"AudioScheduledSourceNode"
-,"AudioParamMap"
-,"WheelEvent"
-,"VisualViewport"
-,"VTTCue"
-,"UIEvent"
-,"TransitionEvent"
-,"TextEvent"
-,"TaskAttributionTiming"
-,"StylePropertyMapReadOnly"
-,"StylePropertyMap"
-,"Selection"
-,"SecurityPolicyViolationEvent"
-,"Screen"
-,"Response"
-,"ResizeObserverEntry"
-,"ResizeObserver"
-,"Request"
-,"ReportingObserver"
-,"PointerEvent"
-,"MutationEvent"
-,"MouseEvent"
-,"MediaQueryListEvent"
-,"MediaQueryList"
-,"MediaList"
-,"LayoutShift"
-,"LargestContentfulPaint"
-,"KeyframeEffect"
-,"KeyboardEvent"
-,"IntersectionObserverEntry"
-,"IntersectionObserver"
-,"InputEvent"
-,"InputDeviceCapabilities"
-,"IdleDeadline"
-,"Headers"
-,"HTMLCollection"
-,"FontFaceSetLoadEvent"
-,"FontFace"
-,"FocusEvent"
-,"FileReader"
-,"FileList"
-,"File"
-,"DOMRectReadOnly"
-,"DOMRectList"
-,"DOMQuad"
-,"DOMMatrixReadOnly"
-,"DOMMatrix"
-,"CompositionEvent"
-,"Blob"
-,"AnimationEvent"
-,"AnimationEffect"
-,"Animation"
-,"WebKitCSSMatrix"
-,"WebKitMutationObserver"
-,"personalbar"
-,"origin"
-,"external"
-,"screen"
-,"innerWidth"
-,"innerHeight"
-,"scrollX"
-,"pageXOffset"
-,"scrollY"
-,"pageYOffset"
-,"visualViewport"
-,"screenX"
-,"screenY"
-,"outerWidth"
-,"outerHeight"
-,"devicePixelRatio"
-,"clientInformation"
-,"event"
-,"offscreenBuffering"
-,"screenLeft"
-,"screenTop"
-,"defaultStatus"
-,"defaultstatus"
-,"styleMedia"
-,"isSecureContext"
-,"queueMicrotask"
-,"requestAnimationFrame"
-,"cancelAnimationFrame"
-,"captureEvents"
-,"releaseEvents"
-,"requestIdleCallback"
-,"cancelIdleCallback"
-,"getComputedStyle"
-,"matchMedia"
-,"moveTo"
-,"moveBy"
-,"resizeTo"
-,"resizeBy"
-,"scroll"
-,"scrollTo"
-,"scrollBy"
-,"getSelection"
-,"find"
-,"fetch"
-,"btoa"
-,"atob"
-,"setTimeout"
-,"clearTimeout"
-,"setInterval"
-,"clearInterval"
-,"createImageBitmap"
-,"blur"
-,"sessionStorage"
-,"localStorage"
-,"chrome"
-,"WebAssembly"
-,"FragmentDirective"
-,"FeaturePolicy"
-,"UserActivation"
-,"XSLTProcessor"
-,"BackgroundFetchManager"
-,"BackgroundFetchRecord"
-,"BackgroundFetchRegistration"
-,"Notification"
-,"PeriodicSyncManager"
-,"EnterPictureInPictureEvent"
-,"PictureInPictureWindow"
-,"PushManager"
-,"PushSubscription"
-,"PushSubscriptionOptions"
-,"RemotePlayback"
-,"VideoPlaybackQuality"
-,"openDatabase"
-,"trustedTypes"
-,"Worklet"
-,"TrustedHTML"
-,"TrustedScript"
-,"TrustedScriptURL"
-,"TrustedTypePolicy"
-,"TrustedTypePolicyFactory"
-,"ApplicationCacheErrorEvent"
-,"caches"
-,"AbsoluteOrientationSensor"
-,"Accelerometer"
-,"Cache"
-,"CacheStorage"
-,"Credential"
-,"CredentialsContainer"
-,"FederatedCredential"
-,"Gyroscope"
-,"Keyboard"
-,"KeyboardLayoutMap"
-,"LinearAccelerationSensor"
-,"NavigationPreloadManager"
-,"OrientationSensor"
-,"PasswordCredential"
-,"RelativeOrientationSensor"
-,"RTCIceTransport"
-,"Sensor"
-,"SensorErrorEvent"
-,"StorageManager"
-,"XRDOMOverlayState"
-,"BarcodeDetector"
-,"filterOut"
+[
+  'MediaCapabilities',
+  'InputDeviceInfo',
+  'DecompressionStream',
+  'CompressionStream',
+  'BlobEvent',
+  'BeforeInstallPromptEvent',
+  'BaseAudioContext',
+  'AudioScheduledSourceNode',
+  'AudioParamMap',
+  'WheelEvent',
+  'VisualViewport',
+  'VTTCue',
+  'UIEvent',
+  'TransitionEvent',
+  'TextEvent',
+  'TaskAttributionTiming',
+  'StylePropertyMapReadOnly',
+  'StylePropertyMap',
+  'Selection',
+  'SecurityPolicyViolationEvent',
+  'Screen',
+  'Response',
+  'ResizeObserverEntry',
+  'ResizeObserver',
+  'Request',
+  'ReportingObserver',
+  'PointerEvent',
+  'MutationEvent',
+  'MouseEvent',
+  'MediaQueryListEvent',
+  'MediaQueryList',
+  'MediaList',
+  'LayoutShift',
+  'LargestContentfulPaint',
+  'KeyboardEvent',
+  'IntersectionObserverEntry',
+  'IntersectionObserver',
+  'InputEvent',
+  'InputDeviceCapabilities',
+  'IdleDeadline',
+  'Headers',
+  'HTMLCollection',
+  'FontFaceSetLoadEvent',
+  'FontFace',
+  'FocusEvent',
+  'FileReader',
+  'FileList',
+  'File',
+  'DOMRectReadOnly',
+  'DOMRectList',
+  'DOMQuad',
+  'DOMMatrixReadOnly',
+  'DOMMatrix',
+  'CompositionEvent',
+  'Blob',
+  'WebKitCSSMatrix',
+  'WebKitMutationObserver',
+  'personalbar',
+  'origin',
+  'external',
+  'screen',
+  'innerWidth',
+  'innerHeight',
+  'scrollX',
+  'pageXOffset',
+  'scrollY',
+  'pageYOffset',
+  'visualViewport',
+  'screenX',
+  'screenY',
+  'outerWidth',
+  'outerHeight',
+  'devicePixelRatio',
+  'clientInformation',
+  'event',
+  'offscreenBuffering',
+  'screenLeft',
+  'screenTop',
+  'defaultStatus',
+  'defaultstatus',
+  'styleMedia',
+  'isSecureContext',
+  'queueMicrotask',
+  'requestAnimationFrame',
+  'cancelAnimationFrame',
+  'captureEvents',
+  'releaseEvents',
+  'requestIdleCallback',
+  'cancelIdleCallback',
+  'getComputedStyle',
+  'matchMedia',
+  'moveTo',
+  'moveBy',
+  'resizeTo',
+  'resizeBy',
+  'scroll',
+  'scrollTo',
+  'scrollBy',
+  'getSelection',
+  'find',
+  'fetch',
+  'btoa',
+  'atob',
+  'setTimeout',
+  'clearTimeout',
+  'setInterval',
+  'clearInterval',
+  'createImageBitmap',
+  'blur',
+  'sessionStorage',
+  'localStorage',
+  'chrome',
+  'WebAssembly',
+  'FragmentDirective',
+  'FeaturePolicy',
+  'UserActivation',
+  'XSLTProcessor',
+  'BackgroundFetchManager',
+  'BackgroundFetchRecord',
+  'BackgroundFetchRegistration',
+  'Notification',
+  'EnterPictureInPictureEvent',
+  'PictureInPictureWindow',
+  'PushManager',
+  'PushSubscription',
+  'PushSubscriptionOptions',
+  'RemotePlayback',
+  'VideoPlaybackQuality',
+  'openDatabase',
+  'trustedTypes',
+  'Worklet',
+  'TrustedHTML',
+  'TrustedScript',
+  'TrustedScriptURL',
+  'TrustedTypePolicy',
+  'TrustedTypePolicyFactory',
+  'ApplicationCacheErrorEvent',
+  'caches',
+  'AbsoluteOrientationSensor',
+  'Accelerometer',
+  'Credential',
+  'CredentialsContainer',
+  'FederatedCredential',
+  'Gyroscope',
+  'Keyboard',
+  'KeyboardLayoutMap',
+  'LinearAccelerationSensor',
+  'NavigationPreloadManager',
+  'OrientationSensor',
+  'PasswordCredential',
+  'RelativeOrientationSensor',
+  'RTCIceTransport',
+  'Sensor',
+  'SensorErrorEvent',
+  'StorageManager',
+  'XRDOMOverlayState',
+  'BarcodeDetector',
+  'filterOut',
+];
