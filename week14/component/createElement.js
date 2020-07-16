35a86fff53f281654efd5d352c4d72b1609015dc
yaoyao1987 <yaolingyan1987@126.com>
2020-07-16 19:24:28 +0800

    fix:为轮播组件添加鼠标操作
    <body>
    
    <footer>
    
        feat(新特性),
        fix(修改问题),
        docs(文档修改),
        style(代码格式修改, 注意不是 css 修改),
        refactor(代码重构),
        test(测试),
        chore(其他修改, 比如构建流程, 依赖管理)
        影响的的范围
        提交描述

---
+++ week14/component/createElement.js
@@ -0,0 +1,70 @@
+export function createElement(Cls, attributes, ...children) {
+  let o;
+  if (typeof Cls === "string") {
+    o = new Wrapper(Cls);
+  } else {
+    o = new Cls({
+      timer: {},
+    });
+  }
+
+  for (const name in attributes) {
+    o.setAttribute(name, attributes[name]);
+  }
+
+  let visit = (children) => {
+    for (let child of children) {
+      if (typeof child === "object" && child instanceof Array) {
+        visit(child);
+        continue;
+      }
+      if (typeof child === "string") {
+        child = new Text(child);
+      }
+      o.appendChild(child);
+    }
+  };
+
+  visit(children);
+  return o;
+}
+
+export class Text {
+  constructor(text) {
+    this.root = document.createTextNode(text);
+  }
+  mountTo(parent) {
+    parent.appendChild(this.root);
+  }
+}
+
+export class Wrapper {
+  constructor(type) {
+    this.children = [];
+    this.root = document.createElement(type);
+  }
+
+  get style() {
+    return this.root.style;
+  }
+
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value);
+  }
+
+  appendChild(child) {
+    this.children.push(child);
+  }
+
+  addEventListener() {
+    this.root.addEventListener(...arguments);
+  }
+
+  mountTo(parent) {
+    parent.appendChild(this.root);
+    if (!this.children.length) return;
+    for (let child of this.children) {
+      child.mountTo(this.root);
+    }
+  }
+}
